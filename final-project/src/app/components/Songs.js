537ba538a0d022b672e39204b309316328b295aa
Joseph <60304239+josephvu10@users.noreply.github.com>
2024-04-08 16:08:39 -0400

    added to songs.js and songs.css

--- final-project/src/app/components/Songs.js
+++ final-project/src/app/components/Songs.js
@@ -5,17 +5,28 @@ import PlayBar from 'PlayBar.js';
 import React, { useState } from'react';
 
 const Songs = () => { //hello friends, i need help with how to show my page in react...
-    return (
-        <div className="songs-container">
-            <NavBar />
-            <img src="https://static.wikia.nocookie.net/rhythmhive/images/7/7b/Get_Up_cover.jpg/revision/latest?cb=20230831203447">Insert Song Image</img>
-            <h3 className="title">Title: Super Shy</h3>
-            <p>Artist: New Jeans</p>
-            <p>Album: </p>
-            <div className='add-song-button'>
-                <button className='add-song-btn'>Add Song</button>
+    return ( //make header and playbar at the bottom as individual react components
+        <div>
+            <div className="songs-container">
+                <div className="song-header"> 
+                    <header className="song-header">
+                        <h1>SoundPalette</h1>
+                        <button className='login-button'>Login</button>
+                    </header>
+                </div>
+                <div className="song-card">
+                    <h2>Song</h2>
+                    <div className="img">
+                        <img src="https://m.media-amazon.com/images/I/51M5a2vdU1L._UXNaN_FMjpg_QL85_.jpg"></img>
+                    </div>
+                    <h3 className="title">Title: Super Shy</h3>
+                    <p>Artist: New Jeans</p>
+                    <p>Album: </p>
+                    <div className='add-song-button'>
+                        <button className='add-song-btn'>Add to Playlist</button>
+                    </div>
+                </div>
             </div>
-            <PlayBar />
         </div>
     );
 };
