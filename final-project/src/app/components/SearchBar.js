20d1b2de1ac0bd0dc1fddd680993dd1e814b151a
Kimmi Dulaney <kimmidulaney@gmail.com>
2024-04-08 10:32:51 -0400

    created search bar component

---
+++ final-project/src/app/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Search..." />
+      <button type="submit">Search</button>
+    </div>
+  );
+};
+
+
+export default SearchBar;
