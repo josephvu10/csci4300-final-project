0b47802ac97e5c2122c22279212915a7d9550551
Kimmi Dulaney <kimmidulaney@gmail.com>
2024-04-23 22:59:37 -0400

    Changed frontend of login page

--- final-project/pages/login.js
+++ final-project/pages/login.js
@@ -1,11 +1,15 @@
 'use client'
 import React, { useState } from 'react';
 import styles from '../styles/login.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Login = ({ onLogin = username => console.log('Default login attempt:', username), onCreateAccount }) => {
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+
 
   const handleLogin = () => {
     if (username.trim() !== '') {
@@ -20,40 +24,58 @@ const Login = ({ onLogin = username => console.log('Default login attempt:', use
     setLoggedInUser(null);
   };
 
+    // Function to toggle password visibility
+    const togglePasswordVisibility = () => {
+      setShowPassword(!showPassword);
+    };
+
   return (
-    <div className={styles.loginContainer}>
-      <header>
-        <h2>Login</h2>
+    <>
+      <header className={styles.header}>
+      <img src="Images/logo2.png" alt="Company Logo" className="logo" />
       </header>
-      <div>
+      <div className={styles.loginContainer}>
         {loggedInUser ? (
-          <>
+          <div className={styles.card}>
             <p>Welcome, {loggedInUser}!</p>
             <button className={styles.logoutBtn} onClick={handleLogout}>Logout</button>
-          </>
+          </div>
         ) : (
-          <div>
-            <label className={styles.label}>Username:</label>
-            <input 
-              type="text" 
-              value={username} 
-              onChange={(e) => setUsername(e.target.value)} 
-              className={styles.inputField}
-            />
-            <label className={styles.label}>Password:</label>
-            <input 
-              type="password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-              className={styles.inputField}
-            />
-            <button className={styles.loginButton} onClick={handleLogin}>Login</button>
-            <p>Don't have an account?</p>
-            <button className={`${styles.loginButton} ${styles.createAccountBtn}`} onClick={onCreateAccount}>Create Account</button>
+          <div className={styles.card}>
+            <h2> Log in to SoundPalette </h2>
+            <hr className={styles.divisionLine} />
+            <div className={styles.inputContainer}>
+              <label className={styles.label}>Username:</label>
+              <input 
+                type="text" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                className={styles.inputField}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label className={styles.label}>Password:</label>
+              <div className={styles.passwordInputWrapper}>
+              <input 
+                type={showPassword ? "text" : "password"} // Toggle between text and password type
+                // type="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                className={styles.inputField}
+              />
+              <div className={styles.togglePassword} onClick={togglePasswordVisibility}>
+                  <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+                </div>
+                </div>
+            </div>
+            <button className={styles.loginButton} onClick={handleLogin}>Log In</button>
+            <hr className={styles.divisionLine} />
+            <p>Don't have an account? <a className={styles.signUpLink} onClick={onCreateAccount}>Sign up for SoundPalette</a></p>
+          {/* <button className={`${styles.loginButton} ${styles.createAccountBtn}`} onClick={onCreateAccount}>Create Account</button> */}
           </div>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
