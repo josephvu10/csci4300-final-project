7bc4f5f475ca604ec9933e8883a7a4b71da08fd3
Joseph <60304239+josephvu10@users.noreply.github.com>
2024-04-25 00:10:17 -0400

    attempted to connect backend but backend does run test backend

--- final-project/src/app/components/login/page.js
+++ final-project/src/app/components/login/page.js
@@ -1,14 +1,27 @@
-'use client'
-import React, { useState } from 'react';
-import Link from 'next/link';
-import styles from './login.module.css';
+"use client";
+import Link from "next/link";
+import styles from "./login.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 //import LinkedLogo from "./linkedLogo";
+import { useState, useContext } from 'react';
+
+//Stephens code
+/*
 import axios from 'axios'; // Import Axios
+import { useState, useContext } from 'react';
+import UserContext from '../../context/UserContext';
+import { useRouter } from 'next/navigation';
+
+*/
+const Login = // () => {
+({
+  onLogin = (username) => console.log("Default login attempt:", username),
+  onCreateAccount,
+}) => {
 
 
-const Login = ({ onLogin = username => console.log('Default login attempt:', username), onCreateAccount }) => {
+
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -40,20 +53,69 @@ const Login = ({ onLogin = username => console.log('Default login attempt:', use
   };
 
 
+  /*
+  const router = useRouter();
+  const { userData, setUserData } = useContext(UserContext);
+
+  //redirect if user is already logged in
+  useEffect(() => {
+    if (userData.token) {
+      router.push("/"); // redirect
+    }
+  }, [userData.token, router]);
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      //send login request to the server
+      const response = await axios.post(
+        "http://localhost:8086/login",
+        formData
+      );
+      setUserData({
+        token: response.data.token,
+        user: response.data.user,
+      });
+      //store the authentication token in local storage
+      localStorage.setItem("auth-token", response.data.token);
+      router.push("/");
+    } catch (error) {
+      console.error("Login failed:", error);
+      //handle login error
+    }
+  };
+
+  */
+
   return (
     <>
-
       <div className={styles.loginContainer}>
         {loggedInUser ? (
           <div className={styles.card}>
             <p>Welcome, {loggedInUser}!</p>
-            <button className={styles.logoutBtn} onClick={handleLogout}>Logout</button>
+            <button className={styles.logoutBtn} onClick={handleLogout}>
+              Logout
+            </button>
           </div>
         ) : (
           <div className={styles.card}>
             <h2> Log in to SoundPalette </h2>
             <hr className={styles.divisionLine} />
-            {error && <p className={styles.error}>{error}</p>} {/* Display error message */}
+            {error && <p className={styles.error}>{error}</p>}{" "}
+            {/* Display error message */}
             <div className={styles.inputContainer}>
               <label className={styles.label}>Username</label>
               <input
@@ -74,26 +136,32 @@ const Login = ({ onLogin = username => console.log('Default login attempt:', use
                   onChange={(e) => setPassword(e.target.value)}
                   className={styles.inputField}
                 />
-                <div className={styles.togglePassword} onClick={togglePasswordVisibility}>
+                <div
+                  className={styles.togglePassword}
+                  onClick={togglePasswordVisibility}
+                >
                   <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
                 </div>
               </div>
             </div>
-            <button className={styles.loginButton} onClick={handleLogin}>Log In</button>
+            <button className={styles.loginButton} onClick={handleLogin}>
+              Log In
+            </button>
             <hr className={styles.divisionLine} />
-            <p>Don't have an account?
-  <Link href="/createAccount">
-    <span className={styles.signUpLink}> Sign up for SoundPalette</span>
-  </Link>
-</p>
-
-
+            <p>
+              Don't have an account?
+              <Link href="/createAccount">
+                <span className={styles.signUpLink}>
+                  {" "}
+                  Sign up for SoundPalette
+                </span>
+              </Link>
+            </p>
           </div>
         )}
       </div>
     </>
   );
 };
-
 
 export default Login;
