6ec2b1e1a82a9437710499b7e839cebecff6b7ee
Kimmi Dulaney <kimmidulaney@gmail.com>
2024-04-25 06:57:49 -0400

    updated search bar

--- final-project/src/app/components/search/page.js
+++ final-project/src/app/components/search/page.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./SearchBar.css";  
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = () => {
   return (
     <div className="search-container">
-      <input type="text" placeholder="Search..." />
-      <button type="submit">Search</button>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search-input"
+      />
+      <div className="search-icon">
+        <FontAwesomeIcon icon={faSearch} />
+      </div>
     </div>
   );
 };
-
 
 export default SearchBar;
