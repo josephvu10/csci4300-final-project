36838550ef66ed11281978b72d544b9d34722365
Kimmi Dulaney <kimmidulaney@gmail.com>
2024-04-25 06:12:04 -0400

    edited front end of homepage

--- final-project/src/app/page.js
+++ final-project/src/app/page.js
@@ -4,45 +4,51 @@ import styles from "./page.module.css";
 import SearchBar from "./components/search/page";
 import NavigationBar from "./components/navBar/page";
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 export default function Home() {
 
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const images = ['s1.jpg', 's2.jpg', 's3.jpg']; 
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
-    }, 5000); // Change slide every 5 seconds
-
-    return () => clearInterval(interval);
-  }, []);
+  const [currentSlide, setCurrentSlide] = useState(Array(6).fill(0));
+  const images = [
+    ['cover1.png', 'cover2.png', 'cover3.png'],
+    ['cover4.png', 'cover5.png', 'cover6.png'],
+    ['cover7.png', 'cover8.png', 'cover9.png'],
+    ['cover10.png', 'cover11.png', 'cover12.png'],
+    ['cover13.png', 'cover14.png', 'cover15.png'],
+    ['cover16.png', 'cover17.png', 'cover18.png'],
+  ]; 
 
   return (
-      <main>
-      <NavigationBar />
-      <SearchBar />
-      <div className={styles.welcomeBanner}>
-        <div className={styles.welcomeTextContainer}> 
+    <div className={styles.hpContainer}>
+    <nav className={styles.navBar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link href='/components/createAccount'>
+            <button className={styles.navButton}>Create Account</button>
+          </Link>
+        </li>
+        <li className={styles.navItem2}>
+          <Link href='/components/login'> 
+            <button className={styles.navButton}>Login</button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    
+    <div className={styles.welcomeBanner}>
+      <div className={styles.welcomeTextContainer}> 
         <div className={styles.welcomeText}>
-                        <h1> Welcome to SoundPalette! </h1>
-                        <p> Where playlists paint your mood... </p>
-                    </div>
+          <h1> Welcome to <br /> SoundPalette </h1>
+          <p> Where playlists paint your mood </p>
         </div>
-        <img src="Images/welcomeBanner.jpg" alt="banner"/>
       </div>
+      <div className={styles.overlay}></div>
+      <img src="Images/HPBanner.gif" alt="banner" className={styles.bannerImage} />
+    </div>
+    <div className={styles.searchBarContainer}>
+      <SearchBar />
+    </div>
+  </div>
 
-      <div className={styles.slideshow}>
-          {images.map((image, index) => (
-            <img
-              key={index}
-              src={`Images/${image}`}
-              alt={`Slide ${index + 1}`}
-              className={`${styles.slide} ${currentSlide === index ? styles.active : ''}`}
-              style={{ animationDelay: `${index * 5}s` }}
-            />
-          ))}
-        </div>
-      </main>
-  );
-}
+);
+};
