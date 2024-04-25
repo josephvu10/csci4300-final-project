5cc2d7dba4ae69e4e9b087aa0f407b5208631eb7
Kimmi Dulaney <kimmidulaney@gmail.com>
2024-04-25 11:28:15 -0400

    added an animated photo grid on homepage

--- final-project/src/app/page.js
+++ final-project/src/app/page.js
@@ -8,15 +8,45 @@ import Link from 'next/link';
 
 export default function Home() {
 
-  const [currentSlide, setCurrentSlide] = useState(Array(6).fill(0));
+  const [currentSlide, setCurrentSlide] = useState(Array(8).fill(0));
+  const [displayedImages, setDisplayedImages] = useState([]);
+
   const images = [
-    ['cover1.png', 'cover2.png', 'cover3.png'],
-    ['cover4.png', 'cover5.png', 'cover6.png'],
-    ['cover7.png', 'cover8.png', 'cover9.png'],
-    ['cover10.png', 'cover11.png', 'cover12.png'],
-    ['cover13.png', 'cover14.png', 'cover15.png'],
-    ['cover16.png', 'cover17.png', 'cover18.png'],
+    'cover1.png', 'cover2.png', 'cover3.png',
+    'cover4.png', 'cover5.png', 'cover6.png',
+    'cover7.png', 'cover8.png', 'cover9.png',
+    'cover10.png', 'cover11.png', 'cover12.png',
+    'cover13.png', 'cover14.png', 'cover15.png',
+    'cover16.png', 'cover17.png', 'cover18.png',
   ]; 
+
+  useEffect(() => {
+    // Change displayed images initially
+    changeDisplayedImages();
+
+    // Set interval to change displayed images every 3 seconds
+    const interval = setInterval(() => {
+      changeDisplayedImages();
+    }, 5000);
+
+    // Clear interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  const changeDisplayedImages = () => {
+    const randomImages = [];
+    const availableImages = [...images]; // Create a copy of the images array
+
+    for (let i = 0; i < 8; i++) {
+      const randomIndex = Math.floor(Math.random() * availableImages.length);
+      const selectedImage = availableImages[randomIndex];
+      randomImages.push(selectedImage);
+      // Remove the selected image from the available images
+      availableImages.splice(randomIndex, 1);
+    }
+    
+    setDisplayedImages(randomImages);
+  };
 
   return (
     <div className={styles.hpContainer}>
@@ -48,6 +78,20 @@ export default function Home() {
     <div className={styles.searchBarContainer}>
       <SearchBar />
     </div>
+    <p> See what's trending near you</p>
+
+    <div className={styles.gridContainer}>
+        {displayedImages.map((imageName, index) => (
+          <div key={index} className={styles.gridCell}>
+            <div className={styles.imageContainer}>
+              <Image src={`/images/${imageName}`} alt={`Image ${index}`} width={150} height={150} />
+            </div>
+          </div>
+        ))}
+      </div>
+
+
+      
   </div>
 
 );
